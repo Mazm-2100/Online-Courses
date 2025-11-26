41c5dc2cac7b2df33bfff15812a5a27355ae3f64
mazm-2100 <mazmgoo@gmail.com>
2025-11-26 15:39:25 +0200

    second

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,10 +4,10 @@ import { GiOpenBook } from "react-icons/gi";
 export default function Footer() {
   return (
     <footer className="bg-gradient-to-r from-indigo-800 to-indigo-950 w-full text-white mt-10 p-8 py-16 flex justify-center items-center">
-      <div className="container w-90/100 h-full">
-        <div className="container mx-auto px-4">
-          <div className="flex flex-col-reverse md:flex-row  justify-center md:justify-between items-center  md:items-center gap-8 md:gap-4">
-            <div className="flex flex-col items-center justify-center">
+      <div className="container wfull  md:w-95/100 lg:w-85/100 h-full">
+        <div className="w-full mx-auto px-4">
+          <div className="flex flex-col-reverse md:flex-row  w-full justify-center md:justify-between items-center  md:items-center gap-8 md:gap-4">
+            <div className="flex  flex-col items-center justify-center">
               <div className="flex p-2 gap-2 mb-2 items-center">
                 <GiOpenBook className="text-6xl font-bold text-white" />
                 <h2 className="text-5xl font-bold text-white ">CourseWave</h2>
@@ -17,7 +17,7 @@ export default function Footer() {
                 reserved.
               </p>
             </div>
-            <div className="flex items-center h-1/2 w-full lg:w-1/2 gap-8 md:gap-4">
+            <div className="flex bg-amber-800 items-center h-1/2 w-full lg:w-1/2 gap-8 md:gap-4">
               <div className="flex flex-col items-center w-1/2">
                 <h3 className="text-2xl font-semibold text-white mb-3">
                   Quick Links
